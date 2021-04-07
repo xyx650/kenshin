0de02ed355740819c3701deed55567d504346fb3
richard <richard.lamn@hotmail.com>
2021-04-07 17:25:56 +0800

    feat: update md2bak

--- scripts/md2bak.js
+++ scripts/md2bak.js
@@ -4,10 +4,9 @@ const filePath = './components/'
 
 // 忽略清单
 const ignoresMap = {
-  button: ['block', 'danger'],
+  button: [],
   card: []
 }
-
 
 const index = args.findIndex(i => i === 'true' || i === 'false')
 const isIgnoreBasic = index > -1 ? String(args.splice(index, 1)) : 'true'
@@ -17,6 +16,7 @@ const keys = Object.keys(ignoresMap)
 const ignoreList = []
 fs.readdir(filePath, (err, components) => {
   !err && components.forEach(component => {
+
     !component.includes('.') && fs.readdir(filePath + component + '/demo/', (err, files) => {
       isIgnoreBasic === 'true' && ignoreList.push(filePath + component + '/demo/basic.md')
       !err && files.forEach(file => {
@@ -29,6 +29,22 @@ fs.readdir(filePath, (err, components) => {
           !error && console.log('成功修改' + oldPath)
           error && console.log(error)
         })
+      })
+    })
+
+    !component.includes('.') && fs.readdir(filePath + component, (err, files) => {
+      !err && files.forEach(file => {
+        if (file.indexOf('index.zh-CN') != -1) {
+          let suffixName = file.split('.')[2]
+          suffixName = suffixName === 'md' ? (sufName || 'bak') : (sufName || 'md')
+          const oldPath = filePath + component + '/' + file
+          const newPath = filePath + component + '/index.zh-CN.' + suffixName
+          keys.includes(component) && keys.forEach(key => ignoresMap[key].includes('index') && ignoreList.push(oldPath))
+          !ignoreList.includes(oldPath) && fs.rename(oldPath, newPath, error => {
+            !error && console.log('成功修改' + oldPath)
+            error && console.log(error)
+          })
+        }
       })
     })
   })
