e9924dbd7017656df571c76ab129a6a5f8a14c4e
xyx650 <475815694@qq.com>
2021-04-07 16:25:58 +0800

    feat: add change .md files to other script

--- scripts/md2bak.js
+++ scripts/md2bak.js
@@ -1,27 +1,28 @@
 const fs = require('fs')
-const isIgnoreBasic = true // 是否忽略basic
+const args = process.argv.slice(2)
+const filePath = './components/'
+
 // 忽略清单
 const ignoresMap = {
   button: ['block', 'danger'],
 }
 
-const filePath = './components/'
 
+const index = args.findIndex(i => i === 'true' || i === 'false')
+const isIgnoreBasic = index > -1 ? String(args.splice(index, 1)) : 'true'
+let sufName = args.find(s => s !== 'true' || s !== 'false')
 
-
-
-
-const sufName = process.argv[2]
 const keys = Object.keys(ignoresMap)
 const ignoreList = []
 fs.readdir(filePath, (err, components) => {
   !err && components.forEach(component => {
     !component.includes('.') && fs.readdir(filePath + component + '/demo/', (err, files) => {
-      isIgnoreBasic && ignoreList.push(filePath + component + '/demo/basic.md')
+      isIgnoreBasic === 'true' && ignoreList.push(filePath + component + '/demo/basic.md')
       !err && files.forEach(file => {
-        const [fileName] = file.split('.')
+        let [fileName, suffixName] = file.split('.')
+        suffixName = suffixName === 'md' ? (sufName || 'bak') : (sufName || 'md')
         const oldPath = filePath + component + '/demo/' + file
-        const newPath = filePath + component + '/demo/' + fileName + '.' + sufName
+        const newPath = filePath + component + '/demo/' + fileName + '.' + suffixName
         keys.includes(component) && ignoresMap[keys].includes(fileName) && ignoreList.push(oldPath)
         !ignoreList.includes(oldPath) && fs.rename(oldPath, newPath, error => {
           !error && console.log('成功修改' + oldPath)
