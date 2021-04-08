9654b54f2f0202e13eceb80e7e58020d3697b8e5
xyx650 <475815694@qq.com>
2021-04-08 10:35:41 +0800

    feat: 修改 md script

--- scripts/md2bak.js
+++ scripts/md2bak.js
@@ -2,11 +2,11 @@
  * 忽略清单
  ****************/
 const ignoresMap = {
-  button: ['index'],
-  checkbox: ['index'],
-  card: []
+  button: ['index', 'block'],
+  checkbox: ['index', 'a'],
+  card: [],
+  icon: ['index']
 }
-
 
 
 const fs = require('fs')
@@ -14,53 +14,67 @@ let args = process.argv.slice(2)
 const filePath = './components/'
 
 let argsStr = args.join(',')
+// 是否强制更新，忽略ignoresMap
 const isForce = argsStr.indexOf('!') !== -1
 argsStr = argsStr.replace('!', '')
-
+// 是否忽略入口，index.zh-CN.md
 const isIgnoreIndex = argsStr.indexOf('+') === -1 && !isForce
 argsStr = argsStr.replace('+', '')
 args = argsStr.split(',')
-
+// 是否忽略 demo/basic.md，default:true
 const index = args.findIndex(i => i === 'true' || i === 'false')
 const isIgnoreBasic = index > -1 ? String(args.splice(index, 1)) : 'true'
+// 传入的后缀名
 let sufName = args.find(s => s !== 'true' || s !== 'false')
 
 const keys = Object.keys(ignoresMap)
-const ignoreList = []
-fs.readdir(filePath, (err, components) => {
-  !err && components.forEach(component => {
+const ignoreSet = new Set([])
 
-    !component.includes('.') && fs.readdir(filePath + component + '/demo/', (err, files) => {
-      isIgnoreBasic === 'true' && ignoreList.push(filePath + component + '/demo/basic.md')
-      !err && files.forEach(file => {
-        let [fileName, suffixName] = file.split('.')
-        suffixName = suffixName === 'md' ? (sufName || 'bak') : (sufName || 'md')
-        const oldPath = filePath + component + '/demo/' + file
-        const newPath = filePath + component + '/demo/' + fileName + '.' + suffixName
-        !isForce && keys.includes(component) && keys.forEach(key => ignoresMap[key].includes(fileName) && ignoreList.push(oldPath))
-        !ignoreList.includes(oldPath) && fs.rename(oldPath, newPath, error => {
-          !error && console.log('成功修改' + oldPath)
-          error && console.log(error)
-        })
+const readdir = path => new Promise((resolve, reject) => {
+  fs.readdir(path, (err, files) => {
+    err ? reject(err) : resolve(files)
+  })
+})
+
+const rename = (oldPath, newPath) => new Promise((resolve, reject) => {
+  fs.rename(oldPath, newPath, err => {
+    err ? reject(err) : resolve()
+  })
+})
+
+
+readdir(filePath).then(async components => {
+  // 忽略_开头的文件夹和index.tsx
+  const componentsDir = components.filter(component => !component.startsWith('_') && !component.includes('.'))
+  componentsDir.forEach(dir => {
+    // 入口文件
+    !isIgnoreIndex && readdir(filePath + dir).then(files => {
+      files.forEach(file => {
+        let [fileName, midName, suffixName] = file.split('.')
+        // index.zh-CN.md
+        if (suffixName && midName.includes('-')) {
+          // 设置默认后缀名
+          suffixName = suffixName === 'md' ? (sufName || 'bak') : (sufName || 'md')
+          const oldPath = filePath + dir + '/' + file
+          const newPath = filePath + dir + `/${fileName}.${midName}.` + suffixName
+          !isForce && keys.includes(dir) && ignoresMap[dir].includes('index') && ignoreSet.add(oldPath)
+          !ignoreSet.has(oldPath) && rename(oldPath, newPath).then(() => console.log('成功修改' + oldPath)).catch(e => console.log(e))
+        }
       })
     })
 
-    if (!isIgnoreIndex) {
-      !component.includes('.') && fs.readdir(filePath + component, (err, files) => {
-        !err && files.forEach(file => {
-          if (file.indexOf('index.zh-CN') !== -1 || file.indexOf('index.en-US') !== -1) {
-            let [fileName, midName, suffixName] = file.split('.')
-            suffixName = suffixName === 'md' ? (sufName || 'bak') : (sufName || 'md')
-            const oldPath = filePath + component + '/' + file
-            const newPath = filePath + component + `/${fileName}.${midName}.` + suffixName
-            !isForce && keys.includes(component) && keys.forEach(key => ignoresMap[key].includes('index') && ignoreList.push(oldPath))
-            !ignoreList.includes(oldPath) && fs.rename(oldPath, newPath, error => {
-              !error && console.log('成功修改' + oldPath)
-              error && console.log(error)
-            })
-          }
-        })
+    // demo 目录
+    readdir(filePath + dir + '/demo/').then(files => {
+      // 是否忽略 basic
+      isIgnoreBasic === 'true' && ignoreSet.add(filePath + dir + '/demo/basic.md')
+      files.forEach(file => {
+        let [fileName, suffixName] = file.split('.')
+        suffixName = suffixName === 'md' ? (sufName || 'bak') : (sufName || 'md')
+        const oldPath = filePath + dir + '/demo/' + file
+        const newPath = filePath + dir + '/demo/' + fileName + '.' + suffixName
+        !isForce && keys.includes(dir) && keys.forEach(key => ignoresMap[key].includes(fileName) && ignoreSet.add(oldPath))
+        !ignoreSet.has(oldPath) && rename(oldPath, newPath).then(() => console.log('成功修改' + oldPath)).catch(e => console.log(e))
       })
-    }
+    })
   })
 })
